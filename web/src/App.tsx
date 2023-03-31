a593a101976f69115b4756cc6dbe59b472f889e2
DevMiner <devminer@devminer.xyz>
2023-03-31 13:34:27 +0200

    impl: creation of posts

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,6 +8,43 @@ type Post = {
   timestamp: number;
 };
 
+const Form = () => {
+  const [username, setUsername] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+
+        await fetch("https://backend.thedevminertv.workers.dev", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            content,
+          }),
+        });
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Content"
+        onChange={(e) => setContent(e.target.value)}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
 
@@ -21,8 +58,9 @@ function App() {
 
   return (
     <div className="App">
+      <Form />
       {posts.map((post) => (
-        <div className="post">
+        <div className="post" key={post.id}>
           <h2>{post.username}</h2>
           <p>{post.content}</p>
         </div>
